75c49295f383af7d7e5ce76653d63a470128ddec
Brendan Rocks <rocks.brendan@gmail.com>
2015-06-21 00:07:04 -0700

    Some basic tests

--- tests/testthat/test-brocks.R
+++ tests/testthat/test-brocks.R
@@ -1,8 +1,33 @@
 context("brocks")
 
-## TODO: Rename context
-## TODO: Add more tests
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# html tri ----------------------------------------------------------------
+
+test_that("html_tri", {
+
+  ht1 <- html_tri(1:-1)
+
+  ht2 <- html_tri(1:-1, symbols = c("up" = "&#128522;", "down" = "&#128542;",
+                                    "nochange" = "&#128528;"))
+
+  exp_res2 <-
+    c("<a style='color:green'>&#128522;</a><a>1</a>",
+      "<a style='color:black'>&#128528;</a><a>0</a>",
+      "<a style='color:red'>&#128542;</a><a>-1</a>" )
+
+  exp_res1 <-
+    c("<a style='color:green'>&#9650;</a><a>1</a>",
+      "<a style='color:black'>&#9632;</a><a>0</a>",
+      "<a style='color:red'>&#9660;</a><a>-1</a>")
+
+  expect_equal(ht1, exp_res1)
+  expect_equal(ht2, exp_res2)
+
 })
+
+test_that("misc", {
+  expect_equal(ac_se(c(TRUE, FALSE)), 0.219089, tolerance = 0.002)
+
+
+})
+
