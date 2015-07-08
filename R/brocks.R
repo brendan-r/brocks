6ccca35bfe930b314cb0df96b586788289128519
Brendan Rocks <rocks.brendan@gmail.com>
2015-07-07 20:08:56 -0700

    Added: gg_footnote

--- R/brocks.R
+++ R/brocks.R
@@ -739,6 +739,82 @@ char_cols <- function(x, all = FALSE){
 }
 
 
+
+# Plotting ----------------------------------------------------------------
+
+#' Add a footnote to a ggplot chart
+#'
+#' Currently, the text wrapping is based on the plot's default font size (which
+#' is usually 12pts), meaning that using something smaller (as is the default in
+#' this function), is likely to result in text which is slightly more narrow
+#' than the available horizontal space. I don't mind it. Someone smarter than I
+#' might be able to fix it with splitTextGrob!
+#'
+#' @param p A ggplot2 plot object
+#' @param text The text used for the footnote
+#' @param detect_font for ggplot2 objects, should the function attempt to
+#'   extract the default font used?
+#' @param wrap Should line breaks be added to the text to (attempt) to prevent
+#'   it overflowing?
+#' @param newlines Should a new line be appended to the start and end of the
+#'   string, for spacing?
+#' @param fontface Passed to \code{\link{gpar}}
+#' @param fontsize Passed to \code{\link{gpar}}
+#' @param col Passed to \code{\link{gpar}}
+#' @param fontfamily Passed to \code{\link{gpar}}
+#' @param ... Passed to \code{\link{gpar}}
+#' @export
+#' @examples
+#' \dontrun{
+#'   # Let's make a simple plot and add a footnote to it.
+#'
+#'   # Some footnote text, taken from the 'fortunes' package
+#'   foot_note <- "R is Open Source and so you can modify it to emulate the bugs
+#'   in other software: that is not one of the aims of its developers so please
+#'   don't expect us to do so for you.\n  -- Brian D. Ripley (answering a request
+#'   for a change to Excel-like non IEC 60559 standard conform rounding)
+#'   \n     R-help (March 2009)"
+#'
+#'   # A ggplot2 chart
+#'   library(ggplot2)
+#'   d <- data.frame(mtcars)
+#'   p <- ggplot(d, aes(cyl, mpg)) +
+#'   geom_point() +
+#'   theme(text = element_text(family = "Garamond"))
+#'
+#'   # Let's add our footnote!
+#'   g <- gg_footnote(p, foot_note)
+#'   g
+#'
+#'   # Note that the text is horizontally truncated. It will take up the full
+#'   # width if we use the default ggplot2 font size of 12pts
+#'   g <- gg_footnote(p, foot_note, fontsize = 12)
+#'   g
+#' }
+gg_footnote <- function(p, text = "This is a footnote", detect_font = TRUE,
+                        wrap = TRUE, newlines = TRUE, fontface = "italic",
+                        fontsize = 8, col = "grey50", fontfamily = NULL, ...){
+
+  if(detect_font)
+    fontfamily <- p$theme$text$family
+
+  if(newlines)
+    text <- paste0("\n", text, "\n")
+
+  if(wrap)
+    text <- RGraphics::splitString(text)
+
+  gridExtra::arrangeGrob(
+    p,
+    sub = grid::textGrob(
+      text, x = 0.05, hjust = 0, vjust = 0.5,
+      gp = grid::gpar(fontface = fontface, fontsize = fontsize, col = col,
+                fontfamily = fontfamily, ...))
+  )
+}
+
+
+
 #' An idealised test data set, for demonstrating some of the functions
 #'
 #' An idealised test data set, for demonstrating some of the functions in the
