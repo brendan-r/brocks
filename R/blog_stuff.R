abdabbd38cdcfb8afb5d0b4bee6c03cba1aad5c1
Brendan Rocks <rocks.brendan@gmail.com>
2015-11-14 00:50:58 -0800

    Added htmlwidgets_deps

--- R/blog_stuff.R
+++ R/blog_stuff.R
@@ -206,3 +206,78 @@ blog_opts <- function(...){
     ...
   )
 }
+
+
+#' Configure htmlwidgets dependencies for a knitr-jekyll blog
+#'
+#' This is really just added to build.R.
+#'
+#' @param a The file path for the input file being knit
+#' @param knit_meta The dependencies object.
+#'
+#' @return Used for it's side effects.
+#' @export
+htmlwidgets_deps <- function(a, knit_meta = knitr::knit_meta(),
+                             lib_dir      = "htmlwidgets_deps",
+                             includes_dir = "_includes/htmlwidgets/",
+                             always       = FALSE) {
+
+  # If the directories don't exist, create them
+  dir.create(lib_dir,      showWarnings = FALSE, recursive = TRUE)
+  dir.create(includes_dir, showWarnings = FALSE, recursive = TRUE)
+
+  # Copy the libraries from the R packages to the 'htmlwidgets_deps' dir, and
+  # obtain the html code required to import them
+  deps_str <- html_dependencies_to_string(knit_meta, lib_dir,
+                                          ".")
+
+  # Jekyll markdown posts are prefixed with a 12 char ISO date and hypen, before
+  # becoming html posts. Remove!
+  n12 <- function(x) {
+    substr(x, 12, nchar(x))
+  }
+
+  # Write the html dependency import code to a file, to be imported by the
+  # liquid templates
+  deps_file <- paste0(includes_dir,
+                      gsub(".Rmd$", ".html", n12(basename(a[1]))))
+
+  # Write out the file if either, the dependencies string has anything to add,
+  # or, if the always parameter has been set to TRUE (useful for those building
+  # with GitHub pages)
+  if(always | !grepl("^[[:space:]]*$", deps_str))
+    writeLines(deps_str, deps_file)
+}
+
+
+#' @keywords internal
+#' Adapted from rmarkdown:::html_dependencies_as_string
+html_dependencies_to_string <- function (dependencies, lib_dir, output_dir) {
+  if (!is.null(lib_dir)) {
+    dependencies <- lapply(
+      dependencies, htmltools::copyDependencyToDir, lib_dir
+    )
+
+    dependencies <- lapply(
+      dependencies, htmltools::makeDependencyRelative, output_dir
+    )
+  }
+
+  # A function to add Jekyll boilerplate
+  prepend_baseurl <- function(path){
+    # If the url doesn't start "/", make sure that it does
+    path <- ifelse(!grepl("^/", path),
+                   paste0("/", path),
+                   path
+                   )
+
+    paste0('{{ "', path, '" | prepend: site.baseurl }}')
+  }
+
+  htmltools::renderDependencies(
+    dependencies, "file",
+    encodeFunc = identity,
+    hrefFilter = prepend_baseurl
+  )
+}
+
