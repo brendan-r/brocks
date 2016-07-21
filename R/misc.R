ef803949a8934d000e83ed7aa2ea5a70f20cc394
Brendan Rocks <rocksb@justin.tv>
2016-07-21 16:28:16 -0700

    [unsci] Round numbers < 1k

--- R/misc.R
+++ R/misc.R
@@ -271,7 +271,7 @@ unsci <- function(x, digits = 1, currency = FALSE, symbol = "$") {
   prefixed <- paste0("", ifelse(
     x >= 1e+03 & x < 1e+06, k(x),
     ifelse(
-      x >= 1e+06 & x < 1e+09, M(x), ifelse(x >= 1e+09, B(x), x)
+      x >= 1e+06 & x < 1e+09, M(x), ifelse(x >= 1e+09, B(x), r(x))
     )
   ))
 
